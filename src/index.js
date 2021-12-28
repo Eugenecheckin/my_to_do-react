9c8242b05a2b181a42f654c31dc28646b07848fd
EvgeniyTTI <evgeniytti@rambler.ru>
2021-12-28 15:45:23 +0300

    createReactTemplete

--- src/index.js
+++ src/index.js
@@ -3,22 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {Wrapper} from './component.style.js'
 
-class Element extends React.Component {  
-  render () {
-      return (
-        <Wrapper>
-          <div>
-            Welcome
-          </div>
-        </Wrapper>        
-      );
-    }
+function ToDoItem({title,id,complete}){
+  return(
+    <li className='todo'>
+      <label>
+        <input
+          type='checkbox'
+          defaultChecked={false}
+        />
+        <span>{title}</span>
+      </label>
+    </li>
+  )
+}
+function ToDoList({todos}){
+  return(
+    <ul>
+     {todos.map(item=><ToDoItem key={item.id} {...item}/>)}
+    </ul>    
+  );
+}
+
+class MyToDo extends React.Component {
+  state = {
+    todos:[{id:1,title:"test",complete:false}]
+  }
+  render()
+   {return (
+    <Wrapper>
+      <header className="header">
+		  <h1>todos</h1>
+		  <input className="new-todo" placeholder="What needs to be done?" autoFocus></input>
+	  </header>
+    <ToDoList todos={this.state.todos}/>
+    </Wrapper> );}  
 }
 
 ReactDOM.render(
-  <Element />,
+  <MyToDo />,
   document.getElementById('root')
 );
-
-
-
