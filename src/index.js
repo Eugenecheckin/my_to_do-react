3fdcda7d937bc171e1ac0da2fc0a6ed6806e8819
EvgeniyTTI <evgeniytti@rambler.ru>
2021-12-29 14:26:16 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import './index.css'
 
-import App from './App';
+import App, {store} from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
