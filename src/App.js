f248aba042c5c7e5c224647ec91e902535addeb8
EvgeniyTTI <evgeniytti@rambler.ru>
2021-12-30 13:25:24 +0300

    addComponents

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Tasks from './components/Tasks.js';
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <div className="input-task">      
         <Postform/>
       </div>
-      <div className='todo-tasks'>
+      <div className="todo-tasks">
         <Tasks tasks = {
-          [ { id : 1, title : 'first' },
-            { id : 2, title : 'second' },
-            { id : 3, title : 'third' }
+          [ { id : 1, title : "first" },
+            { id : 2, title : "second" },
+            { id : 3, title : "third" }
           ]
           }/>
       </div>
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -8,8 +8,11 @@ class Postform extends React.Component {
 			title: ''
 		};
 	}
-	ChangeInputHandler = (event) => { 
+	SubmitHendler = (event) => {
 		event.preventDefault();
+		console.log(this.state.title);
+	}
+	ChangeInputHandler = (event) => { 		
 		event.persist();
 		this.setState(prev => (
 				{...prev, ...{[event.target.name] : [event.target.value]}}
@@ -20,18 +23,18 @@ class Postform extends React.Component {
 		return (
 			<div>
 				<h1>todos</h1>
-				<FormGroup >			
+				<FormGroup onSubmit={this.SubmitHendler}>			
   				<InputLabel htmlFor="input-task__label">What needs to be done?</InputLabel>
   				<Input 
 					  aria-describedby = "input-task__helper"
-						className = 'input-task'
-						type='text'
+						className = "input-task"
+						type="text"
 					  id = 'title'
 						value = {this.state.title}						
-						name = 'title'
-						onChange={this.ChangeInputHandler} 							
+						name = "title"
+						onChange = {this.ChangeInputHandler} 							
 					/>
-					<Button className='input-task__btn'>Добавить</Button>
+					<Button className="input-task__btn" type="submit">Добавить</Button>
 				</FormGroup>
 			</div>		
 		);
