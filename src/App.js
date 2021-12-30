daa338d6568042475589ec5e43dc6bf5b8f7db47
EvgeniyTTI <evgeniytti@rambler.ru>
2021-12-30 11:16:01 +0300

    addComponents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import {Wrapper} from './styles/Component.style.js'
 import React from 'react';
 import Postform from './components/PostForm';
+import Tasks from './components/Tasks.js';
 
 
 function App() {
   return (
-    <div className="App">      
-      <Postform/>
-    </div>
+    <div className='app'>
+      <div className="input-form">      
+        <Postform/>
+      </div>
+      <div className='todo-tasks'>
+        <Tasks/>
+      </div>
+    </div>    
   );
 }
 
---
+++ src/components/Task.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+	return (
+		<div>
+			<h1>task</h1>
+		</div>
+	);
+}
