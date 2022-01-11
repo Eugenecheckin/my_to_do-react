470e6191719880694aeecc856ceba7a463937ad1
EvgeniyTTI <evgeniytti@rambler.ru>
2022-01-11 10:35:52 +0300

    FixBugs and add Style

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,48 +5,44 @@ import { Button, Checkbox, ListItem } from '@mui/material';
 import {ContentContainer} from '../styles/Component.style.js'
 	
 function Task({ task }) {
-	console.log(task);	
+	console.log(task);
+
 	const selector = useSelector(state => 
-		                             state.tasks.find( element => 
-																	 {
-																		 if ( element.id === task.id ) {
-																			 return true; 
-																	   }
-																	 }
-																 )
-															);
+		                 state.tasks.find( element => {
+										   if ( element.id === task.id ) {
+										   return true; 
+										   }
+										 }));
 	const dispatch = useDispatch();
 
 	const CheckBoxClickEvHandler = () => {
 		dispatch( {
-			type: 'set-checked',
+		  type: 'set-checked',
 			payload: { 
-								 id : selector.id,
-								 checked : selector.checked
-							 }																						  
-	    }
-    ) 
+			  id : selector.id,
+				checked : selector.checked
+			}																						  
+	  }) 
 	}
 	const ButtonClickEvHandler = () => {
 		dispatch( {
 			type: 'del-checked',
 			payload: { 
-								 id : selector.id
-							 }																						  
-	    }
-    ) 
+				id : selector.id
+			}																						  
+	  }) 
 	}
 	return (
 		<div className = "todo-item">
 		  <ContentContainer>
 			  <Checkbox
-					onChange= { CheckBoxClickEvHandler() }
+					onChange= { CheckBoxClickEvHandler }
 				/>
 			  <ListItem className = "text-item">
 				  { selector.title }
 			  </ListItem>
 				<Button 
-				 onClick= { ButtonClickEvHandler() }
+				 onClick= { ButtonClickEvHandler }
 				>
 				 &times;
 				</Button>
